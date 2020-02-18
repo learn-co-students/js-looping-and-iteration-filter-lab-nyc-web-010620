8fe277e61c301b0a551f59a0922cfd6abc92f893
adam-xiao <adam.x.xiao@gmail.com>
2020-02-18 16:56:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching(list, name){
+    return list.filter(function(driver){ return driver.toLowerCase() == name.toLowerCase()});
+}
+
+function fuzzyMatch(list, part){
+    return list.filter(function(driver){ return driver.slice(0, part.length) === part});
+}
+
+function matchName(list, name){
+    return list.filter(function(driver) { 
+        return driver.name.toLowerCase() === name.toLowerCase();
+        }
+    )
+}
